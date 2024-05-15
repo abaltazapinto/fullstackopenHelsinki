c16ae9f0444a9552f13f30c4157f4aff31293249
abaltaza <146941254+abaltazapinto@users.noreply.github.com>
2024-05-15 19:58:02 +0100

    up

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,43 +1,61 @@
 import { useState } from 'react'
-import './App.css'
+import PersonForm from './PersonForm';
+import Persons from './Person';
+import Filter from './Filter';
 
-function App() {
-  const [persons, setPersons] = useState([{ name: 'arthur', number: '917624721'  }]);
-  const [newName, setNewName] = useState('')
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '917624721' }]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
+  const handleNumberChange= (event) => {
+    setNewNumber(event.target.value);
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
-
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+  
+  // updated addPerson also now include newNumber
   const addPerson = (event) => {
     event.preventDefault();
-    if(persons.name.some(person => person.name === newName)) {
-      alert('${newName} is already added to phonebook');
+    if(persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
     } else {
-    const person = { name: newName };
+    const person = { name: newName, number: newNumber };
     setPersons(persons.concat(person));
     setNewName('');
+    setNewNumber('');
     }
   };
 
+  //find persons
+  const filteredPersons = search 
+    ? persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    : persons;
+  
   return (
         
       <div>
         <h2>
         Phonebook
         </h2>
-        <form onSubmit={addPerson}>
-          <div>
-            name: <input value={newName} onChange={handleNameChange} />
-            <button type="submit">add</button>
-          </div>
-        </form>
-      <h2>Numbers</h2>
-      number: <input value={newName}/>
-      <hr/>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
-    </div>
+        <Filter value={search} onChange={handleSearchChange}/>
+        <h3>Add a new</h3>
+        <PersonForm
+          newName={newName}
+          newNumber={newNumber}
+          onNameChange={handleNameChange}
+          onNumberChange={handleNumberChange}
+          onSubmit={addPerson}
+          />
+        <h3>Number</h3>
+        <Persons persons={filteredPersons} />
+      </div>
   );
 };
 
---
+++ part2/Phonebook/src/Filter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Filter = ({value, onChange}) => {
+    return (
+        <div>
+            <label htmlFor="filter">Filter shown with:</label>
+            <input id="filter" value={value} onChange={onChange} />
+        </div>
+    );
+};
+
+export default Filter;
